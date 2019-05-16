75ccd33ffb611f7de102dd493a6e946a7e4d39c4
Mark Vonk <markjhvonk@gmail.com>
2019-05-16 16:10:23 +0200

    Finalize Json parse!

--- csvToJson.py
+++ csvToJson.py
@@ -2,23 +2,39 @@ import json
 import csv
 
 # Open the CSV
-csvFile = open( 'data/Veiligheid_data_Rotterdam_raw.csv', 'r' )
+csvFile = open('data/Veiligheid_data_Rotterdam_raw.csv', 'r')
+# Open the Json
 jsonFile = open('data/rotterdam_safety_data.json', 'w')
 
 # Change each fieldname to the appropriate field name.
 reader = csv.DictReader(csvFile)
 
+# Initialize dictionary
+areas = {}
+
 # Parse the CSV into JSON
-# out = json.dumps([ row for row in reader ] )
-
-jsonFile.write('{ \n')
-i = 0
 for row in reader:
-    jsonFile.write(f'"{i}" : ')
-    json.dump(row, jsonFile)
-    jsonFile.write(', \n')
-    i += 1
+    if row['gebied'] == '':
+        continue
 
-jsonFile.write('} \n')
+    # Check if 'gebied' is already in dictionary
+    if row['gebied'] not in areas:
+        areas[row['gebied']] = {}
 
-print("JSON parsed!")
+    # make 'buurt' a new object
+    areas[row['gebied']][row['BUURT85']] = {}
+    # set id
+    areas[row['gebied']][row['BUURT85']]['id'] = row['wijknr']
+
+    # make copy of 'row' data and remove first 3 columns
+    data = row.copy()
+    data.pop('gebied')
+    data.pop('BUURT85')
+    data.pop('wijknr')
+    # set data
+    areas[row['gebied']][row['BUURT85']]['data'] = data
+
+# dump parsed json data in the file
+json.dump(areas, jsonFile)
+
+print('JSON parsed!')
