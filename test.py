ae3c0e0f9063243ce45e5c75c930e2e7422c3b4f
RoelVoordendag <rvoordendag@gmail.com>
2019-05-16 16:39:01 +0200

    shit code

--- test.py
+++ test.py
@@ -1,1 +1,56 @@
-assert sum([1, 1, 4]) == 6, "Should be 6"
+data = {
+    "Charlois": {
+            "Tarwewijk" : {
+                "id": 71,
+                "data": {
+                    "Veiligheidsindex" : 73,
+                    "Veiligheidsindex -subjectief" : 54
+                }
+            },
+            "Carnisse" : {
+                "id": 71,
+                "data": {
+                    "Veiligheidsindex" : 86,
+                    "Veiligheidsindex -subjectief" : 68
+                }
+            }
+    }
+}
+
+# x -> is input value
+# a,b -> hoogste en laagste van de reeks
+# c,d -> in wat voor categorien moet het worden outgeput
+# y de value die hij returnt,
+def mapFromTo(x,a,b,c,d): 
+   y=(x-a)/(b-a)*(d-c)+c
+   return round(y)
+
+for key , district in data.items():
+    
+
+    pass
+
+# index = district["Veiligheidsindex"]
+# Get started with index    
+# Get highest and lowest value out of loop
+
+for gebied, district in data.items():
+    for district, value in district.items():
+        for key, value in value['data'].items():
+            # print(key)
+            # print(value)
+            # overwrite value with new calulated index
+            # print (key)
+            print(key)
+            print(data[gebied][district][key])
+            # data[gebied][district][key] 
+            # print(data)
+            pass
+        pass
+    pass
+
+
+
+# print (mapFromTo(20, 30, 0, 1,5))
+
+# print (data)
