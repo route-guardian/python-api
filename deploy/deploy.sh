3c5022ac631585af381ecce81a07a698732590c4
Mark Vonk <markjhvonk@gmail.com>
2019-05-23 16:06:48 +0200

    New deploy mehtod

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# any future command that fails will exit the script
-set -e
+# # any future command that fails will exit the script
+# set -e
 
-# Lets write the public key of our aws instance
-# eval $(ssh-agent -s)
-# echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+# # Lets write the public key of our aws instance
+# # eval $(ssh-agent -s)
+# # echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
-# ** Alternative approach
-echo -e "$PRIVATE_KEY" > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-# ** End of alternative approach
+# # ** Alternative approach
+# echo -e "$PRIVATE_KEY" > /root/.ssh/id_rsa
+# chmod 600 /root/.ssh/id_rsaA
+# # ** End of alternative approach
 
-# disable the host key checking.
-./deploy/disableHostKeyChecking.sh
+# # disable the host key checking.
+# ./deploy/disableHostKeyChecking.sh
 
-# we have already setup the DEPLOYER_SERVER in our gitlab settings which is a
-# comma seperated values of ip addresses.
-DEPLOY_SERVERS=$DEPLOY_SERVERS
+# # we have already setup the DEPLOYER_SERVER in our gitlab settings which is a
+# # comma seperated values of ip addresses.
+# DEPLOY_SERVERS=$DEPLOY_SERVERS
 
 # lets split this string and convert this into array
 # In UNIX, we can use this commond to do this
@@ -31,5 +31,5 @@ echo "ALL_SERVERS ${ALL_SERVERS}"
 for server in "${ALL_SERVERS[@]}"
 do
   echo "deploying to ${server}"
-  ssh ubuntu@${server} 'bash' < ./deploy/updateAndRestart.sh
+  ssh -i ${PRIVATE_KEY} ubuntu@${server} 'bash' < ./deploy/updateAndRestart.sh
 done
--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -1,36 +1,40 @@
-#!/bin/bash
+# #!/bin/bash
 
-# any future command that fails will exit the script
-set -e
+# # any future command that fails will exit the script
+# set -e
 
-# Delete the old repo
-rm -rf /home/ubuntu/python_api/
+# # Delete the old repo
+# rm -rf /home/ubuntu/python_api/
 
-# clone the repo again
+# # clone the repo again
+# git clone https://gitlab.com/route-guardian/python-api.git
+
+# #source the nvm file. In an non
+# #If you are not using nvm, add the actual path like
+# # PATH=/home/ubuntu/node/bin:$PATH
+# # source /home/ubuntu/.nvm/nvm.sh
+
+# # # stop the previous pm2
+# # pm2 kill
+# # npm remove pm2 -g
+
+
+# # #pm2 needs to be installed globally as we would be deleting the repo folder.
+# # # this needs to be done only once as a setup script.
+# # npm install pm2 -g
+# # # starting pm2 daemon
+# # pm2 status
+
+# cd /home/ubuntu/python_api
+
+# #install npm packages
+# echo "Running pip install"
+# pip install flask
+# pip install PyMongo
+
+# #Restart the flask server
+# python start.py
+
+sudo su gitlab
+mkdir python-api
 git clone https://gitlab.com/route-guardian/python-api.git
-
-#source the nvm file. In an non
-#If you are not using nvm, add the actual path like
-# PATH=/home/ubuntu/node/bin:$PATH
-# source /home/ubuntu/.nvm/nvm.sh
-
-# # stop the previous pm2
-# pm2 kill
-# npm remove pm2 -g
-
-
-# #pm2 needs to be installed globally as we would be deleting the repo folder.
-# # this needs to be done only once as a setup script.
-# npm install pm2 -g
-# # starting pm2 daemon
-# pm2 status
-
-cd /home/ubuntu/python_api
-
-#install npm packages
-echo "Running pip install"
-pip install flask
-pip install PyMongo
-
-#Restart the flask server
-python start.py
