ea8f3fe245efdeb75019e505a945632f6159b91d
Mark Vonk <markjhvonk@gmail.com>
2019-05-23 17:44:38 +0200

    Stupid bug fix!!!

--- deploy/gitlab-deploy.sh
+++ deploy/gitlab-deploy.sh
@@ -4,7 +4,7 @@ set -f
 ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
 echo "ALL_SERVERS ${ALL_SERVERS}"
 #Iterate servers for deploy and pull last commit
-or server in "${ALL_SERVERS[@]}"
+for server in "${ALL_SERVERS[@]}"
 do
   echo "Deploying to ${server}"
   ssh ubuntu@${server} 'bash' < ./deploy/updateAndRestart.sh
