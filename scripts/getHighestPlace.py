2e0867efb67e8045082e195799d32af96d59be11
RoelVoordendag <rvoordendag@gmail.com>
2019-06-03 09:24:48 +0200

    get latest record

--- scripts/getHighestPlace.py
+++ scripts/getHighestPlace.py
@@ -4,6 +4,9 @@ client = MongoClient('mongodb://localhost:27017/')
 
 db = client.safetyScore
 collection = db.safetyScore
-
-for post in collection.find({}):
+# Get the latest item
+for post in collection.find({}).limit(1).sort([( '$natural', -1 )]):
     print(post)
+
+
+# Sort : https://stackoverflow.com/questions/4110665/sort-nested-dictionary-by-value-and-remainder-by-another-value-in-python
