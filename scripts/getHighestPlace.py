c2a8fc84b34fbf5ace919521e058640461a22a27
RoelVoordendag <rvoordendag@gmail.com>
2019-06-07 09:13:39 +0200

    further shizzle

--- scripts/getHighestPlace.py
+++ scripts/getHighestPlace.py
@@ -5,9 +5,19 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client.safetyScore
 collection = db.safetyScore
 # Get the latest item
-for post in collection.find({}).limit(1).sort([( '$natural', -1 )]):
-    for key, value in post.items():
-        print(key, value)
-    pass
+for post in collection.find({}, {'_id': False}).limit(1).sort([( '$natural', -1 )]):
+    for gebied, value in post.items():
+        for wijk, value in value.items():
+            print(value['data'])
+        pass
+    pass    
 
 # Sort : https://stackoverflow.com/questions/4110665/sort-nested-dictionary-by-value-and-remainder-by-another-value-in-python
+
+# Write psuede code
+# 
+# 1. Count together all the scores per wijk
+# 2. check the endscore
+# 3. compore this to all the different wijken
+# 4. sorteer dit en zet de hoogste boven aan.   
+#    
