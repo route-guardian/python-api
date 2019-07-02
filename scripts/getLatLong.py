193a9aba49b8fc892b1d78807faa1b72322b9d51
Roel Voordendag <rvoordendag@gmail.com>
2019-07-02 21:26:00 +0200

    testing for mark

--- scripts/getLatLong.py
+++ scripts/getLatLong.py
@@ -1,9 +1,14 @@
 import requests
-from .config import apiKeyz
+import json
+from .config import apiKey
+
+with open('../data/crime.json') as data_file:
+    crime = json.load(data_file)
+    print(crime)
 
 # In data there is start point and endpoint
 def getLatLong(data):
     # url : https://api.mapbox.com/directions/v5/mapbox/driving/-122.42,37.78;-77.03,38.91?access_token=pk.eyJ1Ijoicm9lbHZvb3JkZW5kYWciLCJhIjoiY2p3dWkwaXYwMHo3ajN5cXYyeGEya3J5dyJ9.7jDGpM77HWMGOk-P6-Aqfw
     response = requests.get("https://api.mapbox.com/directions/v5/mapbox/driving/" + str(data["startPoint"]["long"]) + "," +  str(data["startPoint"]["lat"]) + ";" + str(data["endPoint"]["long"]) + "," +  str(data["endPoint"]["lat"]) +"?access_token=" + apiKey).json()
 
-    print(response["waypoints"])
+    # print(response["waypoints"])
