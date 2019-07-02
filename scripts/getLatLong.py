18ee1218ea22a2114521b0a815a90e590e001c5a
Roel Voordendag <rvoordendag@gmail.com>
2019-07-02 23:19:58 +0200

    avoid area calculation

--- scripts/getLatLong.py
+++ scripts/getLatLong.py
@@ -1,14 +1,35 @@
 import requests
 import json
-from .config import apiKey
+from .config import APPCODE
+from .config import APPID
 
-with open('../data/crime.json') as data_file:
+
+avoidAreas = []
+
+with open('data/crime.json') as data_file:
     crime = json.load(data_file)
-    print(crime)
+
+    for long, lat in crime["points"]:
+        avoidAreas.append(str(lat))
+        avoidAreas.append(",")
+        avoidAreas.append(str(long))
+        avoidAreas.append(";")
+
+avoidAreas.pop()
+
+stringAvoidArreas = ''.join(avoidAreas)
+
+print(stringAvoidArreas)
+
+ 
+
+
+with open('data/cameras.json') as data_file:
+    cameras = json.load(data_file)
 
 # In data there is start point and endpoint
 def getLatLong(data):
     # url : https://api.mapbox.com/directions/v5/mapbox/driving/-122.42,37.78;-77.03,38.91?access_token=pk.eyJ1Ijoicm9lbHZvb3JkZW5kYWciLCJhIjoiY2p3dWkwaXYwMHo3ajN5cXYyeGEya3J5dyJ9.7jDGpM77HWMGOk-P6-Aqfw
-    response = requests.get("https://api.mapbox.com/directions/v5/mapbox/driving/" + str(data["startPoint"]["long"]) + "," +  str(data["startPoint"]["lat"]) + ";" + str(data["endPoint"]["long"]) + "," +  str(data["endPoint"]["lat"]) +"?access_token=" + apiKey).json()
+    response = requests.get("https://route.api.here.com/routing/7.2/calculateroute.json?app_id=" + APPID + "&app_code=" + APPCODE + "&waypoint0=geo!"+ str(data["startPoint"]["lat"]) +"," + str(data["startPoint"]["long"]) + "&waypoint1=geo!"+ str(data["endPoint"]["lat"]) +"," + str(data["endPoint"]["long"]) + "&mode=fastest;pedestrian;traffic:disabled&avoidareas=" + stringAvoidArreas).json()
 
-    # print(response["waypoints"])
+    print(response)
